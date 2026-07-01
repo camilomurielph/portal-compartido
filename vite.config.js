7c4485e442a0746f21f45bdf7ee208d2fc50fe6e
camilomurielph <149724435+camilomurielph@users.noreply.github.com>
2026-06-30 22:11:56 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portal-compartido/", // Cambia por el nombre de tu repositorio
+  base: "./", // Cambia por el nombre de tu repositorio
 });
